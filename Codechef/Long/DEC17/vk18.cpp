33e78ce4ad9b8be442e7f61923190c7b0fcda2f0
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2017-12-24 16:50:30 +0530

    comments added

--- Codechef/Long/DEC17/vk18.cpp
+++ Codechef/Long/DEC17/vk18.cpp
@@ -1,10 +1,11 @@
     #include<bits/stdc++.h>
     using namespace std;
+    //solve things offline
     int main()
     {
     	long n,i,s,t,e,m=1000001,nn;
     	int x,ee,o;
-    	long f[2*(m+1)];
+    	long f[2*(m+1)];//to store sum of digits
     	long long h[m+1],g[2*(m+1)];
     	g[0]=0;
     	for(i=1;i<=m*2;i++)
@@ -25,15 +26,15 @@
     			}
     			n=n/10;
     		}
-    		f[i]=abs(ee-o);
-    		g[i]=g[i-1]+f[i];
+    		f[i]=abs(ee-o);//sum of digits
+    	   	g[i]=g[i-1]+f[i];//prefix sum of sum of digits
     	}
-    	s=3;
-    	e=4;
+    	s=3;//base case
+    	e=4;//base case
     	h[1]=2;
     	for(i=2;i<=m;i++)
     	{
-    		h[i]=h[i-1]+(g[e]-g[s-1])*2-f[e];
+    		h[i]=h[i-1]+(g[e]-g[s-1])*2-f[e];//logic part
     		e+=2;
     		s++;
     	}
@@ -43,20 +44,4 @@
     		cin>>nn;
     		cout<<h[nn]<<endl;
     	}
-    	
-    	/*for(i=0;i<m;i++)
-    	{
-    		cout<<f[i]<<" ";
-    	}
-    	cout<<endl;
-    	for(i=0;i<m;i++)
-    	{
-    		cout<<g[i]<<" ";
-    	}
-    	cout<<endl;
-    	for(i=0;i<m;i++)
-    	{
-    		cout<<h[i]<<" ";
-    	}
-    	cout<<endl;*/
     } 
