c82518c5c459150e87f0a37c95bb467fabaef854
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-05-22 16:42:15 +0530

    Google

---
+++ LeetCode/389. Find the Difference.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define f(i,s,n) for(long i=s;i<n;i++)
+#define fe(i,s,n) for(long i=s;i<=n;i++)
+#define fr(i,s,n) for(long i=s;i>n;i--)
+#define fre(i,s,n) for(long i=s;i>=n;i--)
+#define mod 998244353
+const ll INF_ll = 1e18;
+const ll INF_l = 1e6 + 1;
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<ll> vll;
+typedef pair<int,int> pi;
+typedef pair<int,double> pid;
+typedef pair<string,int> psi;
+typedef pair<long,long> pl;
+typedef pair<ll,ll> pll;
+typedef set<int> si;
+typedef set<long> sl;
+typedef set<ll> sll;
+typedef multiset<int> msi;
+typedef multiset<long> msl;
+typedef multiset<ll> msll;
+typedef set <pid> spsd;
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int a[150] = {};
+        f(i, 0, s.length())
+            a[s[i]]++;
+        f(i, 0, t.length())
+            a[t[i]]--;
+        f(i, 97, 123)
+            if(a[i] == -1)
+                return i;
+        return 1;
+    }
+};
