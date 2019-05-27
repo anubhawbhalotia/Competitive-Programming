30ecb92bb24bc98b6e5de3447f54eb7bd2587b0b
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-05-27 20:17:52 +0530

    practice Codeforces Round #562 (Div. 2) Lemma - https://www.cut-the-knot.org/proofs/2ColorsOnLine.shtml#solution

---
+++ Codeforces/1169D.cpp
@@ -0,0 +1,89 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef multiset<int> msi;
+typedef multiset<ll> msl;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(v) beg(v), en(v)
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+void solve(int t)
+{
+	string s;
+	cin>>s;
+	ll ans = 0;
+	f(i, 0, s.length())
+	{
+		ll tAns = s.length();
+		int flag = 0;
+		f(k, i, i+10)
+		{
+			f(j, 1, 5)
+			{
+				if(k + j + j >= s.length())
+					break;
+				if(s[k] == s[k + j] && s[k + j] == s[k + j + j])
+				{
+					tAns = min(tAns,(ll)k + (ll)j + (ll)j);
+					flag = 1;
+					break;
+				}
+			}
+		}
+		ans += ((ll)s.length() - tAns);
+	}
+	cout<<ans<<endl;
+}
+void testCase()
+{
+	int t = 1;
+	// cin>>t;
+	f(i, 0, t)
+	{
+		solve(i + 1);
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}  
