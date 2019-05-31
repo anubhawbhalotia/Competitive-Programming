37cc8c1b93363c3b8e76dc3fde997dad62bdfc65
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-05-31 22:07:11 +0530

    practice google

---
+++ LeetCode/274. H-Index.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef multiset<int> msi;
+typedef multiset<ll> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(v) beg(v), en(v)
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+
+class Solution {
+public:
+    int hIndex(vector<int>& ct) {
+        sort(all(ct));
+        int c = 0;
+        fre(i, ct.size() - 1, 0)
+        {
+            if(ct[i] >= (c+1))
+                c++;
+            else
+                break;
+        }
+        return c;
+    }
+};
