40ce70b0b2bc05ac07c7ee99f9b5a4b03a63b9d8
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-05-31 19:08:42 +0530

    practice google

---
+++ LeetCode/128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef multiset<int> msi;
+typedef multiset<ll> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(v) beg(v), en(v)
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set <int> m;
+        f(i, 0, nums.size())
+            m.insert(nums[i]);
+        int ans = 0;
+        f(i, 0, nums.size())
+        {
+            if(m.find(nums[i] - 1) != m.end())
+                continue;
+            int y = 1;
+            while(m.find(nums[i] + y) != m.end())   
+                y++;
+            ans = max(ans, y);
+        }
+        return ans;
+    }
+};
