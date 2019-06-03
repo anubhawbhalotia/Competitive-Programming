8e4027860297557a8201ed71327fe10f6c5e55ac
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-03 18:59:20 +0530

    google practice

---
+++ LeetCode/240. Search a 2D Matrix II.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef multiset<int> msi;
+typedef multiset<ll> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(v) beg(v), en(v)
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int t) {
+        if(m.size() == 0)
+            return false;
+        if(m[0].size() == 0)
+            return false;
+        int a = m.size() - 1, b = 0;
+        while(a >= 0 && b < m[0].size())
+        {
+            if(t > m[a][b])
+                b++;
+            else if(t < m[a][b])
+                a--;
+            else
+                return true;
+        }
+        return false;
+    }
+};
