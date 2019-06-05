c10b7c99abb2fa2ce9b231472eae1e1bbcf4d8d2
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-05 15:33:25 +0530

    virtual Codeforces Round #558 (Div. 2)

---
+++ Codeforces/1163B.cpp
@@ -0,0 +1,119 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+vi f;
+vector <si> s;
+si d;
+vi a;
+int solution(int t)
+{
+	int n;
+	cin>>n;
+	a.resize(n);
+	f.resize(100001, 0);
+	s.resize(100001);
+	f(i, 0, n)
+	{
+		cin>>a[i];
+		f[a[i]]++;
+	}
+	f(i, 1, 100001)
+	{
+		if(f[i])
+		{
+			s[f[i]].insert(i);
+			d.insert(f[i]);
+		}
+	}
+	// cout<<d.size()<<" "<<*d.begin()<<endl;
+	fre(i, n - 1, 0)
+	{
+		// cout<<"i= "<<i<<endl;
+		if(d.size() == 2)
+		{
+			auto it2 = d.begin();
+			auto it1 = it2;
+			it2++;
+			if(*it1 == 1 && s[1].size() == 1)
+				return i + 1;
+			if(*it2 - *it1 == 1 && s[*it2].size() == 1)
+				return i + 1;
+		}
+		else if(d.size() == 1 && (*d.begin() == 1 || s[*d.begin()].size() == 1))
+		{
+			// cout<<"true"<<endl;
+			return i + 1;
+		}
+		s[f[a[i]]].erase(a[i]);
+		if(s[f[a[i]]].empty())
+			d.erase(f[a[i]]);
+		f[a[i]]--;
+		if(f[a[i]])
+		{
+			if(s[f[a[i]]].empty())
+				d.insert(f[a[i]]);
+			s[f[a[i]]].insert(a[i]);
+		}
+	}
+}
+void testCase()
+{
+	int t = 1;
+	// cin>>t;
+	f(i, 0, t)
+	{
+		cout<<solution(i + 1)<<endl;
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}  
---
+++ Codeforces/1163C.cpp
@@ -0,0 +1,118 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+#include <cstddef>
+
+#include <limits>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+vi x, y, n;
+set <pair < pair <  int, int >, int > > s;
+map <pair <int, int>, int> m;
+int gcd(int a, int b)
+{
+	if(b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+void solution(int t)
+{
+	int n, a, b, c, g;
+	ll ans = 0;
+	cin>>n;
+	x.resize(n);
+	y.resize(n);
+	f(i, 0, n)
+	{
+		cin>>x[i]>>y[i];
+		x[i] += 10000;
+		y[i] += 10000;
+	}
+	f(i, 0, n)
+	{
+		f(j, i + 1, n)
+		{
+			a = y[j] - y[i];
+			b = x[j] - x[i];
+			c = (y[i]*x[j]) - (y[j]*x[i]);
+			g = gcd(gcd(a,b), c);
+			a /= g;
+			b /= g;
+			c /= g;
+			s.insert({{a, b}, c});
+		}	
+	}
+	for(auto it = s.begin(); it != s.end(); it++)
+	{
+		if(m.find(it->fi) == m.end())
+			m[it->fi] = 1;
+		else
+			m[it->fi]++;
+	}
+	for(auto it = s.begin(); it != s.end(); it++)
+	{
+		// cout<<it->fi.fi<<" "<<it->fi.se<<" "<<it->se<<endl;
+		ans += (ll)s.size() - (ll)m[it->fi];
+	}
+	cout<<ans/2<<endl;
+
+}
+void testCase()
+{
+	int t = 1;
+	// cin>>t;
+	f(i, 0, t)
+	{
+		solution(i + 1);
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}  
