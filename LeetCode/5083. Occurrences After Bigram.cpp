f23cee0e6cd9ce627d09b79b1473a29e83348d75
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-09 14:46:54 +0530

    Weekly Contest 140

---
+++ LeetCode/5083. Occurrences After Bigram.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef multiset<int> msi;
+typedef multiset<ll> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(v) beg(v), en(v)
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        string temp = "";
+        vector <string> ans, z;
+        f(i, 0, text.size())
+        {
+            if(text[i] != ' ')
+                temp+=text[i];
+            else
+            {
+                ans.pb(temp);
+                temp = "";
+            }
+        }
+        ans.pb(temp);
+        f(i, 0, ans.size() - 2)
+        {
+            if(ans[i] == first && ans[i + 1] == second)
+                z.pb(ans[i + 2]);
+        }
+        return z;
+    }
+};
