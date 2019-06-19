cd89a2dc2ad4effcadc34c2d95c56f5bd8563e55
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-19 16:06:13 +0530

    a2oj dp

---
+++ Codeforces/522B.cpp
@@ -0,0 +1,95 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}   
+void solution(int t)
+{
+	int n;
+	cin>>n;
+	vi w(n + 1), h(n + 1);
+	f(i, 1, n + 1)
+		cin>>w[i]>>h[i];
+	vi ps(n + 1), ss(n + 2), pm(n + 1), sm(n + 2);
+	ps[0] = 0;
+	pm[0] = INT_MIN;
+	ss[n + 1] = 0;
+	sm[n + 1] = INT_MIN;
+	//prefix
+	f(i, 1, n + 1)
+	{
+		ps[i] = ps[i - 1] + w[i];
+		pm[i] = max(pm[i - 1], h[i]);
+	}	
+	//suffix
+	fre(i, n, 1)
+	{
+		ss[i] = ss[i + 1] + w[i];
+		sm[i] = max(sm[i + 1], h[i]);
+	}
+	f(i, 1, n + 1)
+		cout<<(ps[i - 1] + ss[i + 1]) * (max(pm[i - 1], sm[i + 1]))<<" ";
+	cout<<endl;
+}
+void testCase()
+{
+	int t = 1;
+	// cin>>t;
+	f(i, 0, t)
+	{
+		solution(i + 1);
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}  
