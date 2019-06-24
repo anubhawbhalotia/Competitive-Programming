d62b0992d3987e6b48132fc9bcc190840b928817
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-24 09:33:06 +0530

    practice google

---
+++ LeetCode/348. Design Tic-Tac-Toe.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}     
+class TicTacToe {
+public:
+    /** Initialize your data structure here. */
+    vi r, c;
+    int d1, d2, n;
+    TicTacToe(int n) {
+        r.resize(n, 0);
+        c.resize(n, 0);
+        d1 = 0;
+        d2 = 0;
+        this->n = n;
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    int move(int row, int col, int player) {
+        int p = (player == 2) ? -1 : 1;
+        r[row] += p;
+        c[col] += p;
+        if(row == col)  
+            d1 += p;
+        if(row + col == n - 1)
+            d2 += p;
+        if(r[row] == n || c[col] == n || d1 == n || d2 == n || r[row] == -1*n || c[col] == -1*n || d1 == -1*n || -1*d2 == n)
+            return player;
+        return 0;
+    }       
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe* obj = new TicTacToe(n);
+ * int param_1 = obj->move(row,col,player);
+ */
