411d4738acaa3f4e64ac4addf972c2fbdb40be1a
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-26 00:00:14 +0530

    practice google premium

---
+++ LeetCode/281. Zigzag Iterator.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}    
+class ZigzagIterator {
+    int c = 0;
+    vi ans;
+public:
+    ZigzagIterator(vector<int>& v1, vector<int>& v2) {
+        int p = 0, q = 0;
+        while(p < v1.size() && q < v2.size())
+        {
+            if(ans.size() % 2)
+                ans.pb(v2[q++]);
+            else
+                ans.pb(v1[p++]);
+        }
+        while(p < v1.size())
+            ans.pb(v1[p++]);
+        while(q < v2.size())
+            ans.pb(v2[q++]);
+    }
+    int next() {
+        return ans[c++];
+    }
+
+    bool hasNext() {
+        return (c < ans.size());
+    }
+};
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i(v1, v2);
+ * while (i.hasNext()) cout << i.next();
+ */
