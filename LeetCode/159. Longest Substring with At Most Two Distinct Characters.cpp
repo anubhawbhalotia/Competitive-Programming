da4670c434238e4805d4caaa121057112dad30eb
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-06-26 16:59:04 +0530

    practice google premium

---
+++ LeetCode/159. Longest Substring with At Most Two Distinct Characters.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}     
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        vi a(255, 0);
+        int l = 0, r = 0, ans = 0, n = 0, k = 2;
+        while(r < s.length())
+        {
+            a[s[r]]++;
+            if(a[s[r]] == 1)
+                n++;
+            while(n > k)
+            {
+                a[s[l]]--;
+                if(!a[s[l]])
+                    n--;
+                l++;
+            }
+            if(n <= k)
+                ans = max(ans, r - l + 1);
+            r++;
+        }
+        return ans;
+    }
+};
