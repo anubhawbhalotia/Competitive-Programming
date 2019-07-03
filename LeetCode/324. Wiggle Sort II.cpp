bf90cb0e6d220ef4ee4c4d70a96c3fe203e1c9b5
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-07-03 18:20:20 +0530

    O(n) time O(1) space
    find median of array elements in O(1) using median of median pivoting technique
    used 3 way partiotining
    used array rewiring

--- LeetCode/324. Wiggle Sort II.cpp
+++ LeetCode/324. Wiggle Sort II.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<ll> vl;
-typedef pair<int,int> pi;
-typedef pair<ll,ll> pl;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
 typedef set<int> si;
-typedef set<ll> sl;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
 typedef multiset<int> msi;
-typedef multiset<ll> msl;
+typedef multiset<long long> msl;
 #define fi first
 #define se second
 #define mp make_pair
@@ -18,7 +21,7 @@ typedef multiset<ll> msl;
 #define ub upper_bound
 #define beg(x) x.begin()
 #define en(x) x.end()
-#define all(v) beg(v), en(v)
+#define all(x) x.begin(), x.end()
 #define f(i,s,n) for(int i=s;i<n;i++)
 #define fe(i,s,n) for(int i=s;i<=n;i++)
 #define fr(i,s,n) for(int i=s;i>n;i--)
@@ -36,21 +39,80 @@ auto operator+(const pair<T,U> & l, pair<V,W> & r)
 {                                                                                  
     return {l.first+r.first,l.second+r.second};                                    
 }
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}
+int partition(int left, int right, vi &a, int pivot)
+{
+	int j = left, i = left;
+    swap(a[right], a[pivot]);
+	while(i < right)
+	{
+		if(a[i] <= a[right])
+			swap(a[i], a[j++]);
+		i++;
+	}
+    swap(a[j], a[right]);
+	return j;
+}
+int get_optimal_pivot(int left, int right, vi &a)
+{
+	int sample_size = 5;
+	int i = left;
+	while(i <= right)
+	{
+		f(j, i + 1, min(i + sample_size, right + 1))
+		{
+			int k = j;
+			while(k > i && a[k - 1] > a[k])
+			{
+				swap(a[k], a[k - 1]);
+				k--;
+			}
+		}	
+		swap(a[left + ((i - left) / sample_size)], a[min(i + (right - i + 1) / 
+			2 ,i + (sample_size / 2))]);
+		i += sample_size;
+	}
+    int end = left + ((right - left + 1)/sample_size) - 1; 
+    f(i, left + 1, end + 1)
+    {
+        int j = i;
+        while(j > left && a[j - 1] > a[j])
+        {
+            swap(a[j], a[j - 1]);
+            j--;
+        }
+    }
+   return left + ((right - left + 1)/sample_size) / 2;
+}
+int median_of_n_elements(int left, int right, vi &a)
+{
+	if(left == right)
+		return a[left];
+	int pivot = get_optimal_pivot(left, right, a);
+    pivot = partition(left, right, a, pivot);
+   int pos = a.size() / 2;
+	if(pivot < pos)
+		return median_of_n_elements(pivot + 1, right, a);
+	else if(pivot > pos)
+		return median_of_n_elements(left, pivot - 1, a);
+	else
+		return a[pivot];
+}
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vi ans(nums);
-        sort(all(ans));
-        int x = 0;
-        int m = (int)nums.size() & 1;
-        fre(i, floor(((int)nums.size() - 1) / 2.0), m)
-        {
-            nums[x++] = ans[i];
-            nums[x++] = ans[i + (nums.size()) / 2];
-        }
-        if(m)
-        {
-            nums[x++] = ans[0];
-        }
+    int n = nums.size();
+    int mid = median_of_n_elements(0, n - 1, nums);
+    cout<<mid<<endl;
+    #define A(i) nums[(1+2*(i)) % (n | 1)]
+    int i = 0, j = 0, k = n - 1;
+    while (j <= k) {
+        if (A(j) > mid)
+            swap(A(i++), A(j++));
+        else if (A(j) < mid)
+            swap(A(j), A(k--));
+        else
+            j++;
     }
+}
 };
