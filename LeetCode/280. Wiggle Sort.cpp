3f5637bf62c74dfa0b52769096fab074906edabd
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-07-11 10:02:12 +0530

    google practice

---
+++ LeetCode/280. Wiggle Sort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}     
+#define A(x) a[(x * 2 + 1) % (n | 1)]
+class Solution {
+public:
+    void wiggleSort(vector<int>& a) {
+    	int n = a.size();
+        if(!n)
+            return;
+    	nth_element(a.begin(), a.begin() + n / 2, a.end());
+    	int m = a[n / 2], j = 0, d = n - 1, i = 0;
+    	while(i <= d)
+    	{
+    		if(A(i) > m)
+    			swap(A(i++), A(j++));
+    		else if(A(i) < m)
+    			swap(A(i), A(d--));
+            else
+                i++;
+    	}  
+    }
+};
