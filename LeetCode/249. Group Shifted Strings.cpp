9e43f7a906b4e02d8d406eeefd51e718e9bab149
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-07-27 22:43:40 +0530

    practice google

---
+++ LeetCode/249. Group Shifted Strings.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}   
+class Solution {
+public:
+    vector<vector<string>> groupStrings(vector<string>& s) {
+        map <string, int> m;
+        vector<vector<string>> ans(s.size());
+        int x = 0, temp;
+        f(i, 0, s.size())
+        {
+            string t = "";
+            f(j, 0, s[i].length() - 1)
+            {
+                temp = (s[i][j + 1] + 26 - s[i][j]) % 26;
+                t += temp;
+            }
+            if(m.find(t) == m.end())
+            {
+                m[t] = x++;
+            }
+            ans[m[t]].pb(s[i]);
+        }
+        vector<vector<string>> z(ans.begin(), ans.begin() + x);
+        return z;
+    }
+};
