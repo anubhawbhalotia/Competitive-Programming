7892d07bf381085896b32c76976139b356654a47
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-10-20 14:27:22 +0530

    full points

---
+++ Google Kickstart/KS19Gb.cpp
@@ -0,0 +1,137 @@
+     #include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef pair<int,int> pii;
+typedef pair<long long ,long long> pll;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;
+template <typename T,typename U, typename V,typename W>
+auto operator+=(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+} 
+template <typename T,typename U, typename V,typename W>                            
+auto operator+(const pair<T,U> & l, pair<V,W> & r) 
+	-> pair<decltype(l.first+r.first),decltype(l.second+r.second)>                
+{                                                                                  
+    return {l.first+r.first,l.second+r.second};                                    
+}
+template <typename T> T gcd(T a, T b) {return b == 0 ? a : gcd(b, a % b);}   
+string getBinary(ll n)
+{
+	int counter = 0;
+	string temp = "";
+	while(n)
+	{
+		temp += ((n % (ll)2) + 48);
+		n /= (ll)2;
+		counter++;
+	}
+	while(counter++ < 54)
+	{
+		temp += '0';
+	}
+	return temp;
+}
+ll solution(int t)
+{
+	int n;
+	ll  m, a;
+	cin>>n>>m;
+	vector <string> b;
+	f(i, 0, n)
+	{
+		cin>>a;
+		b.pb(getBinary(a));
+	}
+	vl minVal(54);
+	ll val = 1;
+	vi count;
+	f(i, 0, 54)
+	{
+		int c = 0;
+		f(j, 0, n)
+		{
+			c += (b[j][i] - 48);
+		}
+		count.pb(c);
+		c = min(c, n - c);
+		minVal[i] = val * c;
+		val *= 2;
+	}
+	val /= 2;	// val = 2^53
+	vl pref(54, 0);
+	f(i, 1, 54)
+	{
+		pref[i] += (pref[i - 1] + minVal[i - 1]);
+	}
+	ll k = 0;
+	fre(i, 53, 0)
+	{
+		ll temp1 = ((n - count[i]) * val);	//flip
+		ll temp2 = (count[i] * val);		//not flip
+		if(temp1 + pref[i] <= m)
+		{
+			k += val;
+			m -= temp1;
+		}
+		else if(temp2 + pref[i] <= m)
+		{
+			m -= temp2;
+		}
+		else
+		{
+			return -1;
+		}
+		val /= 2;
+	}
+	return k;
+}
+void testCase()
+{
+	int t = 1;
+	cin>>t;
+	f(i, 0, t)
+	{
+		cout<<"Case #"<<i+1<<": "<<solution(i + 1)<<endl;
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}  
