53a0b3de44eac757ceb3deef72891cf74c5bee31
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-11-09 00:59:57 +0530

    practice google

---
+++ LeetCode/1170. Compare Strings by Frequency of the Smallest Character.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findF(string &s)
+    {
+        char m = 'z' + 1;
+        int ans = 0;
+        for(int i = 0; i != s.length(); i++)
+        {
+            if(s[i] < m)
+            {
+                m = s[i];
+                ans = 1;
+            }
+            else if(s[i] == m)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        vector <int> ans, z(13, 0);
+        int f, g;
+        char m;
+        for(int i = 0; i != words.size(); i++)
+        {
+            z[findF(words[i])]++;
+        }
+        for(int i = z.size() - 2; i >= 0; i--)
+        {
+            z[i] = z[i + 1] + z[i];
+        }
+        for(int i = 0; i != queries.size(); i++)
+        {
+            ans.push_back(z[findF(queries[i]) + 1]);
+        }
+        return ans;
+    }
+};
