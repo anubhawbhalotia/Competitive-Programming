d1168efbb6be4ea4a2b34bf2e8cde4570fe8ca7b
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-11-08 22:17:18 +0530

    optimised solution

--- LeetCode/1057. Campus Bikes.cpp
+++ LeetCode/1057. Campus Bikes.cpp
@@ -1,49 +1,36 @@
-class node
-{
-public:
-    int distance;
-    int workerIndex;
-    int bikeIndex;
-    node(int d, int wi, int bi)
-    {
-    	distance = d;
-    	workerIndex = wi;
-    	bikeIndex = bi;
-    }
-    bool operator< (const struct node &right) const
-    {
-    	if(distance != right.distance)
-    		return distance < right.distance;
-    	if(workerIndex != right.workerIndex)
-    		return workerIndex < right.workerIndex;
-    	if(bikeIndex != right.bikeIndex)
-    		return bikeIndex < right.bikeIndex;
-    	return false;
-    }
-
-};
 class Solution {
 public:
     
     vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-        set <node> s;
-        unordered_set <int> w, b;
+        vector <vector<pair<int, int>>> s(2001);
+        vector <int> w(1001, 0), b(1001, 0), ans(workers.size());
         for(int i = 0; i != workers.size(); i++)
         {   
             for(int j = 0; j != bikes.size(); j++)
             {
-                node temp(abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]), i, j);
-                s.insert(temp);
+                s[abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1])].push_back(make_pair(i, j));
             }
         }
-        vector <int> ans(workers.size());
-        for(set <node> :: iterator it = s.begin(); it != s.end(); it++)
+        int count(0);
+        for(int i = 0; i < 2001; i++)
         {
-            if(w.find(it->workerIndex) != w.end() || b.find(it->bikeIndex) != b.end())
-                continue;
-            ans[it->workerIndex] = it->bikeIndex;
-            w.insert(it->workerIndex);
-            b.insert(it->bikeIndex);
+            for(int j = 0; j != s[i].size(); j++)
+            {
+                if(w[s[i][j].first] == 1 || b[s[i][j].second] == 1)
+                {
+                    continue;
+                }
+                ans[s[i][j].first] = s[i][j].second;
+                w[s[i][j].first] = 1;
+                b[s[i][j].second] = 1;
+                count++;
+                if(count == (int)workers.size())
+                    return ans;
+            }
+            if(count == (int)workers.size())
+            { 
+                return ans;
+            }
         }
         return ans;
     }
