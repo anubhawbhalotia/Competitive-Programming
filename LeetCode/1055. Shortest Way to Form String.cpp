34a63c53e90cf5344b6617989b927d8191bed37d
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-11-09 00:24:43 +0530

    optimised solution

--- LeetCode/1055. Shortest Way to Form String.cpp
+++ LeetCode/1055. Shortest Way to Form String.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
     int shortestWay(string source, string target) {
-        vector <int> maxLength(1001, 0), temp(1001, -1);
-        vector <set<int>> v(26);
-        set <int> :: iterator it;
-        int sl = source.length(), tl = target.length();
-        for(int i = 0; i != sl; i++)
+        int ans = 0;
+        for(int i = 0; i != target.length();)
         {
-            v[source[i] - 'a'].insert(i);
-        }
-        for(int k = 0; k != tl; k++)
-        {
-            int i = k, j = -1;
-            while(i != tl)
+            int temp = i;
+            for(int j = 0; j != source.length(); j++)
             {
-                it = v[target[i] - 'a'].upper_bound(j);
-                if(it == v[target[i] - 'a'].end())
+                if(target[i] == source[j])
+                {
+                    i++;
+                }
+                if(i == target.length())
                 {
                     break;
                 }
-                j = *it;
-                if(j == temp[i])
-                {
-                    i = maxLength[k - 1] + 1;
-                    break;
-                }
-                temp[i++] = j;
             }
-            maxLength[k] = i - 1;
-            if(i == k)
+            if(i == temp)
                 return -1;
-        }
-        int flag = maxLength[0], ans = 1, i = 1, M = maxLength[0];
-        while(i != tl)
-        {
-            M = max(M, maxLength[i]);
-            if(i > flag)
-            {
-                flag = M;
+            else
                 ans++;
-            }
-            i++;
         }
         return ans;
+        
     }
 };
