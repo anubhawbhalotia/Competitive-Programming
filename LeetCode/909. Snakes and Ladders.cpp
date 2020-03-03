7e5258c2abac14fcc9a257c79fb61e3de8a4a986
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2020-03-03 16:02:50 +0530

    microsoft practice

--- LeetCode/909. Snakes and Ladders.cpp
+++ LeetCode/909. Snakes and Ladders.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& b) {
-        int n = b.size(), p = n*n;
-        //boustrophedonically mapping
-        int z = 1;
+        int n = b.size(), p = n*n, z = 1;
+        if(p == 1)
+            return 0;
         unordered_map <int,int> m;
         bool par = 1;
         for(int i = (n - 1); i >= 0; i--)
         {
             if(par)
-            {
-                for(int j = 0; j < n; j++)
-                {
-                    m[z++] = b[i][j];
-                }
-            }
+                for(int j = 0; j < n; j++, z++)
+                    m[z] = (b[i][j] != -1) ? b[i][j] : z;
             else
-            {
-                for(int j = n-1; j >=0; j--)
-                {
-                    m[z++] = b[i][j];
-                }
-            }
+                for(int j = n-1; j >=0; j--, z++)
+                    m[z] = (b[i][j] != -1) ? b[i][j] : z;
             par = !par;
         }
         vector <bool> flag(p+1, false);
         queue <int> q;
         q.push(1);
-        flag[1] = true;
-        // int x[] = {1, 2, 3, 4, 5, 6};
-        int x[] = {6, 5, 4, 3, 2, 1};
-        int dis = 1;
+        flag[q.back()] = true;
+        z = 1;
         while(!q.empty())
         {
             int sz = q.size();
@@ -38,34 +28,21 @@ public:
             {
                 int temp = q.front();
                 q.pop();
-                for(auto i : x)
+                for(auto i : {6, 5, 4, 3, 2, 1})
                 {
-                    if(temp + i <= p)
+                    if(m[temp + i] <= p)
                     {
-                        if(m[temp+i] != -1)
+                        if(!flag[m[temp+i]])
                         {
-                            if(!flag[m[temp+i]])
-                            {
-                                flag[m[temp+i]] = true;
-                                q.push(m[temp+i]);
-                            }
+                            if(m[temp+i] == p)
+                                return z;
+                            q.push(m[temp+i]);
+                            flag[q.back()] = true;
                         }
-                        else
-                        {
-                            if(!flag[temp+i])
-                            {
-                                flag[temp+i] = true;
-                                q.push(temp+i);
-                            }
-                        }
-                    }
-                    if(q.empty() == false && q.back() == p)
-                    {
-                        return dis;
                     }
                 }
             }
-            dis++;
+            z++;
         }
         return -1;
     }
