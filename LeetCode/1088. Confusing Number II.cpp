b4f35f759e71fe4ef3b649600348c2e12cd98855
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2020-03-08 18:40:45 +0530

    better solution

--- LeetCode/1088. Confusing Number II.cpp
+++ LeetCode/1088. Confusing Number II.cpp
@@ -1,49 +1,39 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
+    int isValid(int x, unordered_map <int, int> &m)
+    {
+        int temp = x, t = 0;
+        while(x)
+        {
+            if(t > 214748363)
+                return 1;
+            t = t*10 + (m[x%10]);
+            x /= 10;
+        }
+        if(t != temp)
+            return 1;
+        return 0;
+    }
+    void dp(int dig, int &N, vector <int> &a, vector <int> &b, int x, int &ans,unordered_map<int, int> &m)
+    {
+        if(x > N)
+            return;
+        ans += (isValid(x, m));
+        for(int i = (dig == 1) ? 1 : 0; i < 5; i++)
+        {
+            if(x < 214748363)
+                dp(dig+1, N, a, b, (x*10) + a[i], ans, m);
+        }
+    }
     int confusingNumberII(int N) {
-        int i = 1,ans = 0;
-        unordered_map <int, int> s;
+        unordered_map <int, int> m;
         vector <int> a = {0, 1, 6, 8, 9};
         vector <int> b = {0, 1, 9, 8, 6};
         for(int i = 0; i < a.size(); i++)
-            s[a[i]] = b[i];
-        int j, k, t;
-        while(i <= N)
-        {
-            j = 1, k = i, t = 0;
-            while(k)
-            {
-                if(s.find(k % 10) != s.end())
-                {
-                    if(t > 214748363)
-                        t = N+1;
-                    else
-                        t = (t*10) + s[k%10];
-                    k /= 10;
-                    j = (k == 0) ? j : j*10;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            if(!k)
-            {
-                if(t < i)
-                {
-                    ans++;
-                    if(i % 10)
-                        ans++;
-                }
-                else if(t > N)
-                    ans++;
-                i++;
-            }
-            else
-            {
-                i+=j;
-            }
-        }
+            m[a[i]] = b[i];
+        int ans = 0;
+        dp(1, N, a, b, 0, ans, m);
         return ans;
     }
 };
