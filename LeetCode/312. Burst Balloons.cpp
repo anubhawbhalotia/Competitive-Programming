e521569dce85a456b6e3a5e051ba5807dd17c5bd
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2020-03-14 01:42:09 +0530

    optimal substructure

--- LeetCode/312. Burst Balloons.cpp
+++ LeetCode/312. Burst Balloons.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<int> a;
-    vector<vector<int>> DP;
-    int dp(int l, int r)
-    {
-        if(l + 1 == r)
-            DP[l][r] = 0;
-        if(DP[l][r] != -1)
-            return DP[l][r];
-        for(int i = l + 1; i < r; i++)
-        {
-            int temp = a[l] * a[i] * a[r];
-            temp += dp(l, i) + dp(i, r);
-            DP[l][r] = max(DP[l][r], temp);
-        }
-        return DP[l][r];
-    }
     int maxCoins(vector<int>& nums) {
-        a.resize((int)nums.size() + 2, 0);
-        DP.resize((int)a.size(), vector<int>((int)a.size(), -1));
+        int n = nums.size() + 2;
+        vector<int> a(n, 0);
+        vector<vector<int>> DP(n, vector<int>(n, 0));
         a[0] = 1;
-        a[(int)a.size() - 1] = 1;
-        for(int i = 0; i < (int)nums.size(); i++)
+        a[n - 1] = 1;
+        for(int i = 0; i < n - 2; i++)
             a[i+1] = nums[i];
-        return dp(0, (int)a.size() - 1);
+        for(int i = 2; i < n; i++)
+        {
+            for(int j = 0; j < n - i; j++)
+            {
+                for(int k = j + 1; k < j + i; k++)
+                {
+                    DP[j][j+i] = max(DP[j][j+i], DP[j][k] + DP[k][j+i] + a[j] * a[k] * a[j+i]);
+                }
+            }
+        }
+        return DP[0][n-1];
     }
 };
