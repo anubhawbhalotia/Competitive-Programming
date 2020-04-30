7dc2f4cc6bbf9714c87dfb7406cc78710a37d289
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2020-05-01 01:29:40 +0530

    bad problem statement 0) if k == 1, answer is m ^ n 1) if k is even and n == k, answer is m ^ (n / 2) 2) if k is even and n != k, answer is m; 3) if k is odd and n == k, answer id m ^ (n / 2 + 1) 4) if k = odd and n != l, answer is (mC2 * 2) + m;
    if k > n, there are no substring with length k, hence all the strings are possible answer(weird, right?)

---
+++ Codeforces/150B.cpp
@@ -0,0 +1,109 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pt;
+typedef pair<long long ,long long> ptl;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<long long> vl;
+typedef vector<vector<long long>> vvl;
+typedef vector<vector<vector<long long>>> vvvl;
+typedef vector<pair<int, int>> vpt;
+typedef vector<pair<long long, long long>> vptl;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+typedef map<int, int> mi;
+typedef map<long long, long long> ml;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define X first
+#define Y second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 1e9+7;
+int solution(int tc)
+{
+	int n, m, k;
+	cin>>n>>m>>k;
+	ll ans = 1;
+	if(k > n || k == 1)
+	{
+		f(i, 0, n)
+		{
+			ans = (ans * m) % MOD;
+		}
+	}
+	else if(k % 2 == 0)
+	{
+		if(n == k)
+		{
+			f(i, 0, n / 2)
+			{
+				ans = (ans * m) % MOD;
+			}
+		}
+		else
+		{
+			ans = m;
+		}
+	}
+	else
+	{
+		if(n == k)
+		{
+			f(i, 0, n / 2 + 1)
+			{
+				ans = (ans * m) % MOD;
+			}
+		}
+		else
+		{
+			ans = (m * (m - 1)) + m;
+		}
+	}
+	cout<<ans<<endl;
+	return 1;
+}
+void testCase()
+{
+	int tc = 1;
+	// cin>>tc;
+	f(i, 0, tc)
+	{
+		solution(i + 1);
+		// cout<<"Case #"<<i + 1<<": "<<solution(i + 1)<<endl;
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}   
+
