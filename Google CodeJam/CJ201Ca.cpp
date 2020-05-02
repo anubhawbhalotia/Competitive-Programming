6b402dff5e9685d3f972fd682a7bcc7cbd48d789
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2020-05-02 23:13:02 +0530

    if at each second, abs val of row + abs val of column of pepurr's is less than or equal to current time, possible

---
+++ Google CodeJam/CJ201Ca.cpp
@@ -0,0 +1,93 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pt;
+typedef pair<long long ,long long> ptl;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<long long> vl;
+typedef vector<vector<long long>> vvl;
+typedef vector<vector<vector<long long>>> vvvl;
+typedef vector<pair<int, int>> vpt;
+typedef vector<pair<long long, long long>> vptl;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+typedef map<int, int> mi;
+typedef map<long long, long long> ml;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define X first
+#define Y second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;  
+int solution(int tc)
+{
+	int x, y;
+	cin>>x>>y;
+	string m;
+	cin>>m;
+	int a = -y, b = x;
+	int i = 0;
+	map <char, pt> d;
+	d['N'] = {-1, 0};
+	d['E'] = {0, 1};
+	d['S'] = {1, 0};
+	d['W'] = {0, -1};
+	while(i < (abs(a) + abs(b)) && i < m.length())
+	{
+		a += d[m[i]].X;
+		b += d[m[i]].Y;
+		i++;
+		// cout<<i<<" "<<a<<" "<<b<<endl;
+	}
+	if(i >= (abs(a) + abs(b)))
+		return i;
+	else
+		return -1;
+}
+void testCase()
+{
+	int tc = 1;
+	cin>>tc;
+	f(i, 0, tc)
+	{
+		int ans = solution(i + 1);
+		if(ans == -1)
+			cout<<"Case #"<<i + 1<<": IMPOSSIBLE"<<endl;	
+		else
+			cout<<"Case #"<<i + 1<<": "<<ans<<endl;
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}   
