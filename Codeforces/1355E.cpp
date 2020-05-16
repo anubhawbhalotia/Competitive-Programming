693ff7973eed3254ce0284746df77773fa8fe3ec
Anubhaw Bhalotia Inspiron 7567 <bhalotia.anubhaw4@gmail.com>
2020-05-16 21:07:33 +0530

    for a given height, find min cost to build all walls upto that height replace m = a + r if m > (a + r) now, observe that the cost to build for a particular height is a function (decreasing then increasing), with a minima find the minima using binary search

---
+++ Codeforces/1355E.cpp
@@ -0,0 +1,149 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pt;
+typedef pair<long long ,long long> ptl;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<long long> vl;
+typedef vector<vector<long long>> vvl;
+typedef vector<vector<vector<long long>>> vvvl;
+typedef vector<pair<int, int>> vpt;
+typedef vector<pair<long long, long long>> vptl;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+typedef map<int, int> mi;
+typedef map<long long, long long> ml;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+// order_of_key: The number of items in a set that are strictly smaller than k
+// find_by_order: It returns an iterator to the ith largest element
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define X first
+#define Y second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+const int MOD = 998244353;  
+ll getCost(vi &h, ll &t, int &a, int &r, int &m, int d)
+{
+	int n = h.size();
+	ll e = ((ll)d * (ll)n) - (ll)t; 	//diff
+	ll ans = 0;
+	if((a + r) < m)
+	{
+		m = (a + r);
+	}
+	if(e > 0)
+	{
+		ans += ((ll)e * (ll)a);
+		f(i, 0, n)
+		{
+			if(h[i] > d)
+			{
+				ans += ((ll)m * ((ll)h[i] - (ll)d));	
+			}
+		}
+	}
+	else if(e < 0)
+	{
+		e *= -1;
+		ans += ((ll)e * (ll)r);
+		f(i, 0, n)
+		{
+			if(h[i] < d)
+			{
+				ans += ((ll)m * ((ll)d - (ll)h[i]));
+			}
+		}
+	}
+	else
+	{
+		f(i, 0, n)
+		{
+			if(h[i] > d)
+			{
+				ans += ((ll)m * ((ll)h[i] - (ll)d));	
+			}
+		}
+	}
+	return ans;
+}
+int solution(int tc)
+{
+	int n, a, r, m;
+	cin>>n>>a>>r>>m;
+	int b = INT_MAX, B = INT_MIN;
+	vi h(n);
+	ll t = 0;
+	f(i, 0, n)
+	{
+		cin>>h[i];
+		t += h[i];
+		b = min(b, h[i]);
+		B = max(B, h[i]);
+	}
+	int st = b, en = B;
+	while(st < en)
+	{
+		int mid = ((st + en) / 2) + 1;
+		// cout<<st<<" "<<mid<<" "<<en<<endl;
+		ll c = getCost(h, t, a, r, m, mid);
+		ll cPrev = getCost(h, t, a, r, m, mid - 1);
+		// cout<<"cPrev = "<<cPrev<<" c = "<<c<<endl;
+		if(c > cPrev)
+		{
+			en = mid - 1;
+		}
+		else if(c < cPrev)
+		{
+			st = mid;
+		}
+		else
+		{
+			st = mid;
+			break;
+		}
+	}
+	cout<<getCost(h, t, a, r, m, st)<<endl;
+	return 1;
+}
+void testCase()
+{
+	int tc = 1;
+	// cin>>tc;
+	f(i, 0, tc)
+	{
+		solution(i + 1);
+		// cout<<"Case #"<<i + 1<<": "<<solution(i + 1)<<endl;
+	}
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}   
+ 
