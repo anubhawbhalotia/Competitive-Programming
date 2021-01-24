23aeaa4b2016ee2f92fb8c024998e2fe6b3ad077
anubhaw.bhalotia <bhalotia.anubhaw4@gmail.com>
2021-01-24 20:44:16 +0530

    Using snippet

--- Codeforces/122A.cpp
+++ Codeforces/122A.cpp
@@ -1,35 +1,87 @@
-#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>  
+#include <functional>
+#include <bits/stdc++.h>
+using namespace __gnu_pbds;
 using namespace std;
-bool isLuckyNumber(int n) {
-	int digit;
-	while(n) {
-		digit = n % 10;
-		n /= 10;
-		if(digit != 4 && digit != 7) {
-			return 0;
-		}
-	}
+typedef long long ll;
+typedef pair<int,int> pt;
+typedef pair<long long ,long long> ptl;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<long long> vl;
+typedef vector<vector<long long>> vvl;
+typedef vector<vector<vector<long long>>> vvvl;
+typedef vector<pair<int, int>> vpt;
+typedef vector<pair<long long, long long>> vptl;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<long long> sl;
+typedef unordered_set <int> usi;
+typedef unordered_set <long long> usl;
+typedef multiset<int> msi;
+typedef multiset<long long> msl;
+typedef map<int, int> mi;
+typedef map<long long, long long> ml;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>;  
+//Anubhaw Bhalotia https://github.com/anubhawbhalotia
+#define X first
+#define Y second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) x.begin(), x.end()
+#define f(i,s,n) for(int i=s;i<n;i++)
+#define fe(i,s,n) for(int i=s;i<=n;i++)
+#define fr(i,s,n) for(int i=s;i>n;i--)
+#define fre(i,s,n) for(int i=s;i>=n;i--)
+#define watch1(x) cerr <<(#x)<<" = "<<(x)<< endl
+#define watch2(x, y) cerr<<(#x)<<" = "<<(x)<<" "<<(#y)<<" = "<<(y)<<endl
+#define watch3(x, y, z) cerr<<(#x)<<" = "<<(x)<<" "<<(#y)<<" = "<<(y)<<" "<<\
+	(#z)<<" = "<<z<<endl
+#define watchContainer1(x) for(auto i : x){cerr<<i<<" ";}cout<<endl;
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',',\
+	 ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); \
+	err(_it, args); }
+#define log1(x) cout<<x<<endl
+#define max3(a, b, c) max(a, max(b, c))
+#define min3(a, b, c) min(a, min(b, c))
+const int MOD = 998244353;  
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
+
+int solution(int tc)
+{
+	
 	return 1;
 }
-vector<int> getAllLuckyNumberFromOneToN(int n) {
-	vector<int> luckyNumbersList;
-	for(int i = 1; i <= n; i++) {
-		if(isLuckyNumber(i)) {
-			luckyNumbersList.push_back(i);
-		}
+void testCase()
+{
+	int tc = 1;
+	// cin>>tc;
+	f(i, 0, tc)
+	{
+		solution(i + 1);
+		// cout<<"Case #"<<i + 1<<": "<<solution(i + 1)<<endl;
 	}
-	return luckyNumbersList;
 }
-int main() {
-	int n;
-	cin>>n;
-	vector<int> luckyNumbersList = getAllLuckyNumberFromOneToN(n);
-	for(int i : luckyNumbersList) {
-		if(n % i == 0) {
-			cout<<"YES"<<endl;
-			return 0;
-		}
-	}
-	cout<<"NO"<<endl;
-	return 0;
-}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    testCase();
+}   
